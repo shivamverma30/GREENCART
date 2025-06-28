be3855f20c3d7ec6bf7b0037730c41b7d27c8665
Shivam Verma <24204031116@stu.manit.ac.in>
2025-06-28 12:39:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 //allow mutiple origins 
-const allowedOrigins=['http://localhost:5173'];
+const allowedOrigins=['http://localhost:5173','https://vercel.com/shivam-vermas-projects-8078f8a6/greencart];
 
 
 app.post('/stripe',express.raw({type: "application/json"}),stripeWebhooks)

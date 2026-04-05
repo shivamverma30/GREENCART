f6f844367660ee9c452aff833e41218e045156e8
shivam verma <24204031116@stu.manit.ac.in>
2026-04-05 06:30:30 +0530

    API base URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://greencart-backend-psi-five.vercel.app";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: API_BASE_URL,
   withCredentials: true,
 });
 
